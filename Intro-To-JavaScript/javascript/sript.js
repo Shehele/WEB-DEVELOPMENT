6ab131a1541493030b95e7942d13f4f89763ff7b
Bigboss <ochie@Bigboss>
2024-04-06 18:06:12 +0300

    javascript

---
+++ Intro-To-JavaScript/javascript/sript.js
@@ -0,0 +1,3 @@
+let emmanuelFavPhone = "Iphone";
+
+console.log(emmanuelFavPhone); 
---
+++ Intro-To-JavaScript/variables.js
@@ -0,0 +1,7 @@
+let favfood = "Chapati"; //String variable, favfood is the name of the variable and chapati is the the value
+let howManyChapatis = 5; //howManyChapatis is a number/integer variable, and 5 is the current value
+let isHungry = true;
+
+//console.log is a function used to print out values of variables.
+console.log(favfood);
+
