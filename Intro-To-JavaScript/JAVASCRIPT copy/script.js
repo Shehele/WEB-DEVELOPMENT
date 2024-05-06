37a7eb73083adfc03f9af15cdcd3dfd1c0f53fb4
Bigboss <ochie@Bigboss>
2024-05-06 23:07:58 +0300

    javascript

--- Intro-To-JavaScript/JAVASCRIPT copy/script.js
+++ Intro-To-JavaScript/JAVASCRIPT copy/script.js
@@ -1,19 +1,57 @@
-const inputBox =document.getElementById("input-box");
+// Get references to the input box and the list container
+const inputBox =document.getElementById("input-box1");
 const listContainer = document.getElementById("list-container");
 
+// Function to add a task
 function addTask() {
-  if(inputBox.value === ""){
-    alert("Please enter a task ");
+  // check if the input box is empty
+  if(inputBox.value === "Emmanuel"){
+    // If its empty,shown an alert message
+    alert("You must add a task ");
   } else {
+    // If its not empty,create a new list item
     let listItem = document.createElement("li");
+    // Set the text of the list item to the value of the input box
     listItem.textContent = inputBox.value;
+    // Append the list item to the list container
     listContainer.appendChild(listItem);
-
+     // create a close button
     let closeButton = document.createElement("span");
-    closeButton.textContent = "x";
-
+    // set the text of the close button to 'x'
+    closeButton.textContent = "x"; 
+    // Append the close button to the list item
     listItem.appendChild(closeButton);
   }
-
+  // Clear the input box after adding a task
   inputBox.value = "";
+  // Save the updated task list
+  saveTasks();
 }
+
+function handleListContainerClick(event){
+  if (event.target.tagName === "LI"){
+    event.target.classList.toggle("checked");
+    saveTasks();
+  }
+  else if(event.target.tagName === "SPAN"){
+    event.target.parentElement.remove();
+    saveTasks();
+  }
+}
+listContainer.addEventListener("click",handleListContainerClick);
+
+
+
+function saveTasks(){
+  localStorage.setItem("tasks",listContainer.innerHTML);
+}
+
+function showTasks(){
+  let savedTasks = localStorage.getItem("tasks")
+
+if(savedTasks){
+  listContainer.innerHTML = savedTasks;
+ }
+}
+
+showTasks();
